04d851daa4ca4f8ca0bbee73c86cfac42823f325
mariomegulearn <mariomegu.learn@gmail.com>
2025-11-14 22:15:33 -0500

    cv

---
+++ CV/js/script.js
@@ -0,0 +1,10 @@
+        tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        'primary-blue': '#3b82f6',
+                        'accent-blue': '#1e40af',
+                    },
+                }
+            }
+        }
